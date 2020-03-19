74683bf4b69a9a9b9effac6b2420e94e1efafc6b
Neylton Benjamim <neylton.b@hotmail.com>
2020-03-19 18:34:00 -0300

    exemplos de como usar a class static connection

---
+++ example/connection.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__.'/config.php';
+
+use \Database\Connection;
+$conn = Connection::open([
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'dbname' => 'databasesv2'
+]);
+echo 'Passando array com os parametros da conexão';
+var_dump($conn);
+echo '<hr>';
+
+$conn = Connection::open('databasesv3');
+echo 'Buscando pelo nome do banco de dados';
+var_dump($conn);
+echo '<hr>';
+
+$conn = Connection::open();
+echo 'Banco default';
+var_dump($conn);
+echo '<hr>';
+
+$result = $conn->query('SELECT * FROM tab_user');
+var_dump($result->fetchAll());
