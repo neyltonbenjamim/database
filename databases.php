3911113e101b4b744a215c21c4cdf6dcc407066a
Neylton Benjamim <neylton.b@hotmail.com>
2020-03-19 18:33:18 -0300

    configurado os banco de dados

---
+++ databases.php
@@ -0,0 +1,26 @@
+<?php
+
+define('DBS',array(
+
+    'databasesv1' => array(
+        'host'   => 'localhost',
+        'user'   => 'root',
+        'pass'   => '',
+        'dbname' => 'databasesv1'
+    ),
+
+    'databasesv2' => array(
+        'host'   => 'localhost',
+        'user'   => 'root',
+        'pass'   => '',
+        'dbname' => 'databasesv2'
+    ),
+
+    'databasesv3' => array(
+        'host'   => 'localhost',
+        'user'   => 'root',
+        'pass'   => '',
+        'dbname' => 'databasesv3'
+    )
+
+));
